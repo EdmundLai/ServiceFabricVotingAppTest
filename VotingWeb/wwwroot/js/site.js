0679ccd53e19d8239e7ed973b547657f0d4753e4
Edmund Lai <edmund.lai.07@gmail.com>
2021-01-15 12:34:10 -0800

    Initial commit

---
+++ VotingWeb/wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿var app = angular.module('VotingApp', ['ui.bootstrap']);
+app.run(function () { });
+
+app.controller('VotingAppController', ['$rootScope', '$scope', '$http', '$timeout', function ($rootScope, $scope, $http, $timeout) {
+
+    $scope.refresh = function () {
+        $http.get('api/Votes?c=' + new Date().getTime())
+            .then(function (data, status) {
+                console.log(data);
+                $scope.votes = data;
+            }, function (data, status) {
+                $scope.votes = undefined;
+            });
+    };
+
+    $scope.remove = function (item) {
+        $http.delete('api/Votes/' + item)
+            .then(function (data, status) {
+                $scope.refresh();
+            })
+    };
+
+    $scope.add = function (item) {
+        var fd = new FormData();
+        fd.append('item', item);
+        $http.put('api/Votes/' + item, fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        })
+            .then(function (data, status) {
+                $scope.refresh();
+                $scope.item = undefined;
+            },
+                function (response, status) {
+                    console.log("bad request");
+                    
+                    console.log(response);
+                    console.log(status);
+                }
+            );
+    };
+}]);
